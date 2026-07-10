b59c116643df516a23af4249cb3aea1bf4e964ca
sagarbe <156514710+sagarbe@users.noreply.github.com>
2026-07-10 20:36:28 +0530

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,31 +1,51 @@
 const orderBtn = document.getElementById("orderBtn");
 
+const uploadInput = document.getElementById("file");
+
+
 orderBtn.addEventListener("click", async () => {
 
-    const file = fileInput.files[0];
+    const file = uploadInput.files[0];
+
 
     if (!file) {
+
         alert("Please select a file.");
+
         return;
+
     }
+
 
     const uploadFileName = Date.now() + "_" + file.name;
 
+
     const { data, error } = await supabaseClient.storage
+
         .from("documents")
+
         .upload(uploadFileName, file);
 
+
     if (error) {
-        alert("File Upload Failed");
+
+        alert(error.message);
+
         console.log(error);
+
         return;
+
     }
 
+
     const fileUrl =
-        "https://xtwffnvrykavuorvzpjj.supabase.co/storage/v1/object/public/documents/" + uploadFileName;
+    "https://xtwffnvrykavuorvzpjj.supabase.co/storage/v1/object/public/documents/" + uploadFileName;
+
 
     alert("File Uploaded Successfully");
 
+
     console.log(fileUrl);
+
 
 });
