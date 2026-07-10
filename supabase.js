fb5e652e69af1e193c210643fdabe9225c073637
sagarbe <156514710+sagarbe@users.noreply.github.com>
2026-07-10 20:17:32 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,6 @@
 const SUPABASE_URL = "https://xtwffnvrykavuorvzpjj.supabase.co";
 
-const SUPABASE_KEY = "तुमची sb_publishable_... key येथे पेस्ट करा";
-
+const SUPABASE_KEY = sb_publishable_VWSP2NLytDqEkbJ2kUHTMQ_x_Xw2UD6
 const supabase = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
