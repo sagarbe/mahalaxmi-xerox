bf3d1820a5d675349ae4b8505ab480837da4b41f
sagarbe <156514710+sagarbe@users.noreply.github.com>
2026-07-10 20:19:28 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,10 @@
+// Supabase Configuration
+
 const SUPABASE_URL = "https://xtwffnvrykavuorvzpjj.supabase.co";
 
-const SUPABASE_KEY = sb_publishable_VWSP2NLytDqEkbJ2kUHTMQ_x_Xw2UD6
-const supabase = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
+const SUPABASE_KEY = sb_publishable_VWSP2NLytDqEkbJ2kUHTMQ_x_Xw2UD6;
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
 );
