fa133798f70ab2d44c4325dbe31c36d973542e52
sagarbe <156514710+sagarbe@users.noreply.github.com>
2026-07-10 22:18:59 +0530

    Create login.js

---
+++ login.js
@@ -0,0 +1,20 @@
+const loginBtn = document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", async () => {
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const { error } = await supabaseClient.auth.signInWithPassword({
+        email,
+        password
+    });
+
+    if (error) {
+        alert(error.message);
+        return;
+    }
+
+    window.location.href = "admin.html";
+
+});
