61d332ed83e48c8119f648594b5dc52a77e5a397
sagarbe <156514710+sagarbe@users.noreply.github.com>
2026-07-10 21:08:59 +0530

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,6 +1,12 @@
 const uploadFileInput = document.getElementById("file");
 const orderButton = document.getElementById("orderBtn");
 
+const serviceInput = document.getElementById("service");
+const copiesInput = document.getElementById("copies");
+
+const printRadios = document.getElementsByName("print");
+const totalDisplay = document.getElementById("totalPrice");
+
 
 orderButton.addEventListener("click", async () => {
 
@@ -9,41 +15,120 @@ orderButton.addEventListener("click", async () => {
 
 
     if (!selectedFile) {
-
         alert("Please select a file");
         return;
-
     }
 
+
+    // File Upload
 
     const newFileName = Date.now() + "_" + selectedFile.name;
 
 
-    const { data, error } = await supabaseClient.storage
+    const { data: uploadData, error: uploadError } =
+        await supabaseClient.storage
         .from("documents")
         .upload(newFileName, selectedFile);
 
 
-    if (error) {
 
-        console.log("Upload Error:", error);
-        alert("Upload Failed");
+    if (uploadError) {
 
+        console.log(uploadError);
+        alert("File Upload Failed");
         return;
 
     }
 
 
-    const { data: urlData } = supabaseClient.storage
+
+    // Get File URL
+
+    const { data: urlData } =
+        supabaseClient.storage
         .from("documents")
         .getPublicUrl(newFileName);
 
 
 
-    console.log("File URL:", urlData.publicUrl);
+    const fileURL = urlData.publicUrl;
 
 
-    alert("File Uploaded Successfully");
+
+    // Print Type
+
+    let printType = "Black & White";
+
+
+    if(printRadios[1].checked){
+
+        printType = "Color";
+
+    }
+
+
+
+    // Amount
+
+    const amountText = totalDisplay.innerHTML.replace("₹","");
+    const amount = parseInt(amountText);
+
+
+
+    // Copies
+
+    const copies = parseInt(copiesInput.value);
+
+
+
+    // Save Order In Database
+
+
+    const { data: orderData, error: orderError } =
+        await supabaseClient
+        .from("orders")
+        .insert([{
+
+            file_name: newFileName,
+
+            file_url: fileURL,
+
+            service: serviceInput.value,
+
+            print_type: printType,
+
+            copies: copies,
+
+            amount: amount,
+
+            payment: "Pending",
+
+            status: "New",
+
+            page_count: 1,
+
+            print_status: "Pending"
+
+
+        }]);
+
+
+
+    if(orderError){
+
+        console.log(orderError);
+        alert("Order Save Failed");
+        return;
+
+    }
+
+
+
+    alert("Order Placed Successfully 🎉");
+
+
+    console.log("Order Saved:", orderData);
+    console.log("File URL:", fileURL);
 
 
 });
