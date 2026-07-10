6a9024bbf885504ba6b6c9c65321cc58eddc97f8
sagarbe <156514710+sagarbe@users.noreply.github.com>
2026-07-10 20:53:24 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,7 +2,7 @@
 
 const SUPABASE_URL = "https://xtwffnvrykavuorvzpjj.supabase.co";
 
-const SUPABASE_KEY = "sb_publishable_VWSP2NLytDqEkbJ2kUHTMQ_x_Xw2UD6";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh0d2ZmbnZyeWthdnVvcnZ6cGpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2ODI3MzUsImV4cCI6MjA5OTI1ODczNX0.kioSLl0LdUUvC02gdCHDstn4J2eBUVW_Nuo7JJeDXjo";
 
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
