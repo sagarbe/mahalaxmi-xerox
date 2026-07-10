4b46ce717eda2823dcb73a9e62090a164943a176
sagarbe <156514710+sagarbe@users.noreply.github.com>
2026-07-10 18:01:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,53 @@
-const printOptions = document.getElementsByName("print");
-const copiesInput = document.getElementById("copies");
-const totalPrice = document.getElementById("totalPrice");
+const fileInput=document.getElementById("file");
 
-function calculatePrice() {
+const fileName=document.getElementById("fileName");
 
-let price = 5;
+const copies=document.getElementById("copies");
 
-if (printOptions[1].checked) {
-price = 10;
+const radios=document.getElementsByName("print");
+
+const total=document.getElementById("totalPrice");
+
+const rate=document.getElementById("rate");
+
+const pages=document.getElementById("pages");
+
+let pageCount=1;
+
+fileInput.addEventListener("change",()=>{
+
+if(fileInput.files.length>0){
+
+fileName.innerHTML=fileInput.files[0].name;
+
 }
 
-let copies = parseInt(copiesInput.value);
-
-totalPrice.innerHTML = "₹ " + (price * copies);
-
-}
-
-printOptions.forEach(radio=>{
-radio.addEventListener("change",calculatePrice);
 });
 
-copiesInput.addEventListener("input",calculatePrice);
+function calculate(){
 
-calculatePrice();
+let price=5;
+
+if(radios[1].checked){
+
+price=10;
+
+}
+
+rate.innerHTML="₹"+price;
+
+let totalAmount=price*pageCount*parseInt(copies.value);
+
+total.innerHTML="₹"+totalAmount;
+
+}
+
+radios.forEach(r=>{
+
+r.addEventListener("change",calculate);
+
+});
+
+copies.addEventListener("input",calculate);
+
+calculate();
