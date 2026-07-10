3316b00f6d0032dd5f658c68559e2c878a35bc8c
sagarbe <156514710+sagarbe@users.noreply.github.com>
2026-07-10 20:45:25 +0530

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,51 +1,92 @@
+const fileInput = document.getElementById("file");
 const orderBtn = document.getElementById("orderBtn");
 
-const uploadInput = document.getElementById("file");
+const copiesInput = document.getElementById("copies");
+const printOptions = document.getElementsByName("print");
 
 
 orderBtn.addEventListener("click", async () => {
 
-    const file = uploadInput.files[0];
-
+    const file = fileInput.files[0];
 
     if (!file) {
-
-        alert("Please select a file.");
-
+        alert("Please select file");
         return;
-
     }
 
 
-    const uploadFileName = Date.now() + "_" + file.name;
+    let price = 5;
+
+    if(printOptions[1].checked){
+        price = 10;
+    }
 
 
-    const { data, error } = await supabaseClient.storage
-
-        .from("documents")
-
-        .upload(uploadFileName, file);
+    let copies = Number(copiesInput.value);
 
 
-    if (error) {
+    const fileName = Date.now() + "_" + file.name;
 
-        alert(error.message);
 
-        console.log(error);
+    // Upload File
 
+    const {data:uploadData, error:uploadError} =
+    await supabase.storage
+    .from("documents")
+    .upload(fileName,file);
+
+
+    if(uploadError){
+
+        alert("Upload Failed");
+        console.log(uploadError);
         return;
 
     }
 
 
     const fileUrl =
-    "https://xtwffnvrykavuorvzpjj.supabase.co/storage/v1/object/public/documents/" + uploadFileName;
+    "https://xtwffnvrykavuorvzpjj.supabase.co/storage/v1/object/public/documents/"
+    + fileName;
 
 
-    alert("File Uploaded Successfully");
+
+    // Save Order
+
+    const {data,error} = await supabase
+    .from("orders")
+    .insert([{
+
+        file_name:file.name,
+
+        file_url:fileUrl,
+
+        print_type:
+        printOptions[1].checked ? "Color" : "Black & White",
+
+        copies:copies,
+
+        amount:price*copies,
+
+        status:"Pending",
+
+        print_status:"Pending"
+
+    }]);
 
 
-    console.log(fileUrl);
+    if(error){
+
+        console.log(error);
+
+        alert("Order Save Failed");
+
+        return;
+
+    }
+
+
+    alert("Order Placed Successfully");
 
 
 });
