134e0a651e96884ab8a727971ffbf22b29b73fa4
sagarbe <156514710+sagarbe@users.noreply.github.com>
2026-07-10 22:06:51 +0530

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,47 @@
+const table = document.getElementById("ordersTable");
+
+async function loadOrders(){
+
+    const { data, error } = await supabaseClient
+        .from("orders")
+        .select("*")
+        .order("id", { ascending:false });
+
+    if(error){
+        console.log(error);
+        return;
+    }
+
+    table.innerHTML = "";
+
+    data.forEach(order=>{
+
+        table.innerHTML += `
+        <tr>
+
+            <td>${order.id}</td>
+
+            <td>
+                <a href="${order.file_url}" target="_blank">
+                    Open File
+                </a>
+            </td>
+
+            <td>${order.service}</td>
+
+            <td>${order.print_type}</td>
+
+            <td>${order.copies}</td>
+
+            <td>₹${order.amount}</td>
+
+            <td>${order.print_status}</td>
+
+        </tr>
+        `;
+
+    });
+
+}
+
+loadOrders();
