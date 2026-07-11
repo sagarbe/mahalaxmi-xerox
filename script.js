d2b551d073d1684b8ff973cd680914a24faa34fa
sagar b <sagarbalange@gmail.com>
2026-07-11 17:24:45 +0530

    Update admin panel

--- script.js
+++ script.js
@@ -8,19 +8,24 @@ const totalDisplay = document.getElementById("totalPrice");
 const rateDisplay = document.getElementById("rate");
 const pagesDisplay = document.getElementById("pages");
 
+
 let pageCount = 1;
 
 let bwPrice = 5;
 let colorPrice = 10;
 
+
 // LOAD PRICE FROM SUPABASE
+
 async function loadPrices() {
 
 
     const { data, error } =
     await supabaseClient
     .from("settings")
-    .select("*");
+    .select("*")
+    .eq("id",1)
+    .single();
 
 
 
@@ -34,76 +39,118 @@ async function loadPrices() {
 
     }
 
-    
-
-    data.forEach(item => {
 
 
-        if(item.key === "bw_price"){
+    bwPrice = Number(data.bw_price);
 
-            bwPrice = Number(item.value);
-
-        }
+    colorPrice = Number(data.color_price);
 
 
-
-        if(item.key === "color_price"){
-
-            colorPrice = Number(item.value);
-
-        }
-
-
-    });
 
     console.log("B/W Price:", bwPrice);
+
     console.log("Color Price:", colorPrice);
+
+
 
     calculate();
 
 
 }
 
+
+
 // FILE SELECT
+
 printFileInput.addEventListener("change", () => {
 
-    if (printFileInput.files.length > 0) {
-        fileNameDisplay.innerText = printFileInput.files[0].name;
-    } else {
-        fileNameDisplay.innerText = "No file selected";
+
+    if(printFileInput.files.length > 0){
+
+        fileNameDisplay.innerText =
+        printFileInput.files[0].name;
+
     }
+    else{
+
+        fileNameDisplay.innerText =
+        "No file selected";
+
+    }
+
 
     pagesDisplay.innerText = pageCount;
 
+
     calculate();
+
 
 });
 
+
+
 // CALCULATE TOTAL
-function calculate() {
+
+function calculate(){
+
 
     let price = bwPrice;
 
-    if (printRadios[1].checked) {
+
+
+    if(printRadios[1].checked){
+
         price = colorPrice;
+
     }
 
-    rateDisplay.innerText = "₹" + price;
 
-    const copies = parseInt(copiesInput.value) || 1;
 
-    const total = price * pageCount * copies;
+    rateDisplay.innerText =
+    "₹" + price;
 
-    totalDisplay.innerText = "₹" + total;
+
+
+    const copies =
+    parseInt(copiesInput.value) || 1;
+
+
+
+    const total =
+    price * pageCount * copies;
+
+
+
+    totalDisplay.innerText =
+    "₹" + total;
+
+
 
 }
 
-// EVENTS
+
+
+// PRINT TYPE CHANGE
+
 printRadios.forEach(radio => {
+
+
     radio.addEventListener("change", calculate);
+
+
 });
 
-copiesInput.addEventListener("input", calculate);
+
+
+// COPIES CHANGE
+
+copiesInput.addEventListener(
+"input",
+calculate
+);
+
+
 
 // START
+
 loadPrices();
